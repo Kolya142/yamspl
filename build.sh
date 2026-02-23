b6e4108b14a4ba8888fc12aee0a8de548c25017a
Kolya142 <nikolasshevelkos@gmail.com>
2026-02-23 18:27:49 +0300

    Something useful.

--- build.sh
+++ build.sh
@@ -1,2 +1,11 @@
-python3 pyx/pyx.py main.py main.pyx
+set -e
+
+if [ ! -e pyx ]; then
+    echo ------ GETTING pyx ------
+    git clone https://github.com/Kolya142/pyx pyx;
+    (cd pyx; python3 stage0/pyx_compiler.py pyx_compiler.py pyx_compiler.pyx);
+    echo "*" > pyx/.gitignore;
+fi
+
+python3 pyx/pyx_compiler.py main.py main.pyx
 mypy main.py
